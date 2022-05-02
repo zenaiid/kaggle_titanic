7a17be7ee856018da738db0dc0d017ddf903a3ab
pronebsd <tomasz.pronobis@campus.tu-berlin.de>
2022-05-02 07:43:22 +0200

    added code skeleton for model

--- titanic_main.py
+++ titanic_main.py
@@ -7,7 +7,9 @@ Created on Fri Apr 29 17:30:56 2022
 import pandas as pd
 from feature_engineering import feature_engineering
 #import numpy as np
+from toch.utils.data import Dataset, DataLoader
 import torch
+from torch import nn, optim
 
 df = pd.read_csv('./data/train.csv', delimiter = ',')
 df = feature_engineering(df)
@@ -16,12 +18,32 @@ df = feature_engineering(df)
 y = torch.tensor(df['Survived'].values)
 X = torch.tensor(df.loc[:, df.columns != 'Survived'].values)
 
+#TODO: put model definitions in function in seperate file 
+# define model
+class LogisticRegression(nn.Module):
+    
+    def __init__(self, input_dim, output_dim):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+        
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+#TODO: Datasets und Dataloader:
+# 1. define test and train datasets where csv's are loaded (outside k-fold loop)
+# 2. define dataloader (outside k-loop)
+# 3. define subsamplers for k-fold cross validation (inside k-loop because dependent on subsamplng indices)  
+# 4. define train & testloaders, with dataset (1.) and subsamplers (3.), specify batch size
+# 5. proceed with standard testing and training, by iterating through train- & test-loaders
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
 
 
 # def batch_split(X, batch):
 #   return (X[batch, :], np.delete(X, batch, 0))
 
- 
 
 # def k_fold(X, y, n_batches, kernel, lambda_reg):
 #   losses = []
